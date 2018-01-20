60c5d92bc568dfd6e2bbf07b204f72c3a1800125
zhangkun <mlu_kun@163.com>
2018-01-20 15:50:34 +0800

    Cloud20180113版

---
+++ App/Admin/Conf/config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'UPDATE_URL' => 'http://update.qd.qiawei.com/',
+    'NEWS_URL' => 'http://Admin.qiawei.com/api/news/',
+    'Version' => '1.1',
+    'IMAGES'=>'./Data/Uploads/Images/'
+);
